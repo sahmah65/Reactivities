fbcb9f110c345ce4d2c68803acd3e18c58996349
Mehdi <Mehdi@DESKTOP-CJ40C2P>
2021-09-03 19:16:32 +0430

    End Of Section 3

---
+++ client-app/src/DuckItem.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+import { Duck } from './demo'
+
+interface Props {
+    duck: Duck;
+}
+
+export default function DuckItem({ duck }: Props) {
+    return (
+        <div  >
+            <span style={{ color: "red" }}> {duck.name} </span>
+            <button onClick={() => duck.makeSound(duck.name + ' quack')}>click to sound</button>
+        </div> 
+        )
+}
