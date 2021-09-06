83d746d6fa4ddedfb638c3f890279cb2ab9eba74
Mehdi <Mehdi@DESKTOP-CJ40C2P>
2021-09-06 23:03:17 +0430

    End Of Section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
 
@@ -12,6 +13,8 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
 
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+   public class Edit
+    {
+        public class Commmand : IRequest
+        {
+            public Activity Activity { set; get; }
+        }
+        public class Handler : IRequestHandler<Commmand>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context,IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+            public async Task<Unit> Handle(Commmand request, CancellationToken cancellationToken)
+            {
+
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
