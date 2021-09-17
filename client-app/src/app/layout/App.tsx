a199793dfb6473a6122f39c24e1f79dbf1b6c726
Mehdi <Mehdi@DESKTOP-CJ40C2P>
2021-09-17 11:36:36 +0430

    End Of Section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,113 +1,32 @@
-import React, {  useEffect, useState } from 'react';
+import React, {useEffect} from 'react';
 
-import { Container} from 'semantic-ui-react'
-import { Activity } from '../models/activity';
+import {Container} from 'semantic-ui-react'
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboards/ActivityDashboard';
-import { v4 as uuid } from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
-import { act } from 'react-dom/test-utils';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-    const [activities, setActivities] = useState <Activity[]>([]);
-    const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-    const [editMode, setEditMode] = useState(false);
-    const [loading, setLoading] = useState(true);
-    const [submitting, setSubmitting] = useState(false);
+    const {activityStore} = useStore();
 
     useEffect(() => {
-        agent.Activities.list().then(response => {
-            let activities: Activity[] = [];
-            response.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                activities.push(activity); 
-            });
-
-            setActivities(activities);
-            setLoading(false);
-        });
-    }, []);
-
-    function handleSelectedActivity(id: string) {
-        setSelectedActivity(activities.find(x=>x.id === id));
-    }
-
-    function handleCancelSelectedActivity() {
-        setSelectedActivity(undefined);
-    }
-
-    function handleFormOpen(id?: string) {
-        id ? handleSelectedActivity(id) : handleCancelSelectedActivity();
-        setEditMode(true);
-    }
-
-    function handleFormClose() {
-        setEditMode(false);
-    }
-
-    function handleCreateOrEditActivity(activity: Activity) {
-        setSubmitting(true);
-
-        if (activity.id) {
-            agent.Activities.update(activity).then(() => {
-                setActivities([...activities.filter(x => x.id !== activity.id), activity])
-                setSelectedActivity(activity);
-                setEditMode(false);
-                setSubmitting(false);
-
-            })
-        } else {
-            activity.id = uuid();
-            agent.Activities.create(activity).then(() => {
-                setActivities([...activities, activity]);
-                setSelectedActivity(activity);
-                setEditMode(false);
-                setSubmitting(false);
-            });
-        }
+        activityStore.loadActivities();
+    }, [activityStore]);
 
 
-       
-    }
-
-    function handleDeleteActivity(id: string) {
-        setSubmitting(true);
-        agent.Activities.delete(id).then(() => {
-            setActivities([...activities.filter(x => x.id !== id)]);
-            setSubmitting(false);
-        })
-       
-    }
-
-    if (loading) { return <LoadingComponent content='Loading App' /> }
+    if (activityStore.loadingInitial) { return <LoadingComponent content='Loading App' /> }
     return (
         <>
            
-                <NavBar openForm={handleFormOpen} />
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}> 
+                <ActivityDashboard />
+            </Container>
 
-            <Container style={{ marginTop: '7em' }}>
-                <ActivityDashboard
-                    activities={activities}
-                    selectedActivity={selectedActivity}
-                    selectActivity={handleSelectedActivity}
-                    cancelSelectActivity={handleCancelSelectedActivity}
-                    editMode={editMode}
-                    openForm={handleFormOpen}
-                    closeForm={handleFormClose}
-                    createOrEdit={handleCreateOrEditActivity}
-                    deleteActivity={handleDeleteActivity}
-                    submitting={submitting}
-                />
-                </Container>
-
-
-
-
-           
          </>
      
   );
 }
 
-export default App;
+export default observer(App);
