96c2295a92efdd585954dcd6e73a600c2754e4c7
Mehdi <Mehdi@DESKTOP-CJ40C2P>
2021-09-26 19:41:43 +0330

    End of section 11

--- client-app/src/features/activities/dashboards/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboards/ActivityListItem.tsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom';
 import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
-
+import { format } from 'date-fns';
 
 
 interface Props {
@@ -30,7 +30,7 @@ export default function ActivityListItem({ activity }: Props) {
 			</Segment>
 			<Segment>
 				<span>
-					<Icon name='clock' />{activity.date}
+					<Icon name='clock' />{format(activity.date!,'dd MM yyyy h:mm aa')}
 					<Icon name='marker' />{activity.venue}
 				</span>
 			</Segment>
