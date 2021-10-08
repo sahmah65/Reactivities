72563dc9d3da013acca0a654d4214add34178c13
Mehdi <Mehdi@DESKTOP-CJ40C2P>
2021-10-08 15:03:31 +0330

    End of section 16

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Activities;
 using Application.Core;
 using Application.Interfaces;
+using Application.Photos;
 using AutoMapper;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -40,7 +42,8 @@ namespace API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
-
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
             return services;
         }
 
---
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -0,0 +1,19 @@
+﻿using Application.Photos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Photos
+{
+   public interface IPhotoAccessor
+    {
+        Task<PhotoUploadResult> AddPhoto(IFormFile file);
+
+        Task<string> DeletePhoto(string publicId);
+
+
+    }
+}
